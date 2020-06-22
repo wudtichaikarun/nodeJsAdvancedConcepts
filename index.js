dcda7b71a0d16d6fee8744cfb2ace048ca7b17bd
romantic <cesc_fabregas1414@hotmail.com>
2020-06-23 04:41:52 +0700

    play with doWork function

--- index.js
+++ index.js
@@ -1,21 +1,38 @@
 const express = require("express");
 const crypto = require("crypto");
 const app = express();
+const cluster = require("cluster");
+// const numCPUs = require("os").cpus().length;
 
-app.get("/", (req, res) => {
-  crypto.pbkdf2("my_password", "my_salt", 100000, 512, "sha512", () => {
-    console.log("request hash");
+if (cluster.isMaster) {
+  const numCPUs = 4;
 
-    res.send("Hi");
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+} else {
+  function doWork(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {}
+  }
+
+  app.get("/", (req, res) => {
+    // crypto.pbkdf2("my_password", "my_salt", 100000, 512, "sha512", () => {
+    //   res.send("Hi");
+    // });
+    console.log(`processId: ${process.pid}  request do work...`);
+    const FIVE_SEC = 5000;
+    doWork(FIVE_SEC);
+    res.send(`Do work processId:${process.pid}`);
   });
-});
 
-app.get("/fast", (req, res) => {
-  res.send("This was fast!");
-});
+  app.get("/fast", (req, res) => {
+    res.send("This was fast!");
+  });
 
-const PORT = 3000;
+  const PORT = 3000;
 
-app.listen(PORT, () => {
-  console.log(`API sever listening on ${PORT} processId: ${process.pid}`);
-});
+  app.listen(PORT, () => {
+    console.log(`API sever listening on ${PORT}  processId: ${process.pid}`);
+  });
+}
